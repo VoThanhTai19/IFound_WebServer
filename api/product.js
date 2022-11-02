2dcae5f31e68a571a5741e43eba5fff206b58c5d
VoThanhTai19 <93961593+VoThanhTai19@users.noreply.github.com>
2022-11-02 11:23:36 +0700

    update

--- api/product.js
+++ api/product.js
@@ -3,21 +3,25 @@ const router = express.Router()
 
 const productController = require('../components/controllers/ProductController');
 
+//Lấy danh sách tất cả sản phẩm
 router.get('/products', async function (req, res, next) {
     const products = await productController.getProducts();
     res.status(200).json(products) 
 }) 
 
+//Lấy chi tiết sản phẩm
 router.get('/:id/product-detail', async function (req, res, next) {
     const product = await productController.getProductById(req.params.id)
     res.status(200).json(product)
 }) 
 
+//Lấy danh sách sản phẩm theo loại sản phẩm
 router.get('/:id/products-by-category', async function (req, res, next) {
     const products = await productController.getProductsByCategoryId(req.params.id);
     res.status(200).json(products)
 })
 
+//Tìm kiếm sản phẩm
 router.post('/product/search', async function (req, res, next) {
 
 })
